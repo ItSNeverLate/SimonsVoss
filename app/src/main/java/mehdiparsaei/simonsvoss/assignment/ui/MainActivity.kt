ef81f2312d3faddf384ec8a9b530f9723735c202
Mehdi <mehdiparsaeigm@gmail.com>
2022-01-22 01:01:42 +0330

    adding view binding

--- app/src/main/java/mehdiparsaei/simonsvoss/assignment/ui/MainActivity.kt
+++ app/src/main/java/mehdiparsaei/simonsvoss/assignment/ui/MainActivity.kt
@@ -1,19 +1,25 @@
 package mehdiparsaei.simonsvoss.assignment.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 import mehdiparsaei.simonsvoss.assignment.R
+import mehdiparsaei.simonsvoss.assignment.databinding.ActivityMainBinding
 
-@EntryPoint
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    val viewModel:MainViewModel by viewModels()
+
+    val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
